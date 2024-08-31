d78b51cd23c1f48c5e496437e510c45e30c1e3f4
Omkar Date <omkardate29@gmail.com>
2024-08-31 20:53:11 +0530

    queue optimized

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
 class Queue {
   #items;
+  #front;
+  #rear;
 
   constructor() {
-    this.#items = [];
+    this.#items = {};
+    this.#front = 0;
+    this.#rear = 0;
   }
 
   enqueue(val) {
-    this.#items.push(val);
+    this.#items[this.#rear] = val;
+    this.#rear++;
   }
 
+  /**
+   * In previous approach we were using array where we used shift method to delete first item in dequeue
+   * which has O(n) time complexity, now here it's O(1) using object
+   */
   dequeue() {
-    this.#items.shift();
+    delete this.#items[this.#front];
+    this.#front++;
   }
 
   peek() {
-    return this.#items[0];
+    return this.#items[this.#front];
   }
 
   isEmpty() {
-    return this.#items.length === 0;
+    return this.#front - this.#rear === 0;
   }
 
   size() {
-    return this.#items.length;
+    return this.#rear - this.#front;
   }
 
   print() {
-    console.log(this.#items.toString());
+    console.log(this.#items);
   }
 }
 
@@ -36,8 +46,11 @@ qu.enqueue(34);
 qu.enqueue(54);
 qu.enqueue(76);
 qu.enqueue(89);
-qu.dequeue();
 
-console.log(qu.peek());
+qu.print()
+
+console.log(qu.peek(), qu.size(), qu.isEmpty());
+
+qu.dequeue();
 
 qu.print()
