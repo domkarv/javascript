a071c237fe8757514fb77eebf9753447adf28f54
Omkar Date <omkardate29@gmail.com>
2024-09-05 18:22:33 +0530

    hashtable implementation but has chances of collision

---
+++ index.js
@@ -0,0 +1,50 @@
+class HashTable {
+  constructor(size) {
+    this.table = new Array(size);
+    this.size = size;
+  }
+
+  hash(key) {
+    let total = 0;
+    for (let i = 0; i < key.length; i++) {
+      total += key.charCodeAt(i);
+    }
+
+    return total % this.size;
+  }
+
+  set(key, value) {
+    const index = this.hash(key);
+    this.table[index] = value;
+  }
+
+  get(key) {
+    const index = this.hash(key);
+    return this.table[index];
+  }
+
+  remove(key) {
+    const index = this.hash(key);
+    this.table[index] = undefined;
+  }
+
+  display() {
+    for (let i = 0; i < this.size; i++) {
+      if (this.table[i]) {
+        console.log(i, this.table[i]);
+      }
+    }
+  }
+}
+
+const ht = new HashTable(50);
+
+ht.set("name", "Omkar Date");
+ht.set("age", 21);
+ht.set("role", "admin");
+
+console.log(ht.get("name"));
+
+ht.remove("role")
+
+ht.display()
