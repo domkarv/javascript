46bdacba21d1b32477fb4d950d71efd5826da588
Omkar Date <omkardate29@gmail.com>
2024-09-12 21:09:54 +0530

    linear search with array's findIndex method

--- index.ts
+++ index.ts
@@ -1,9 +1,5 @@
 function linearSearch(nums: number[], target: number): number {
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === target) return i;
-  }
-
-  return -1;
+  return nums.findIndex((val) => val === target);
 }
 
 const nums = [2, 7, 11, 15, 34, 56, 52, 74, 56, 3];
