0bff08228cef2ff5081dd98ecd5fbdfeb210a53f
Omkar Date <omkardate29@gmail.com>
2024-09-14 03:24:44 +0530

    most common string

--- index.ts
+++ index.ts
@@ -1,20 +1,14 @@
-function binarySearch(
-  nums: number[],
-  target: number,
-  start: number,
-  end: number
-): number {
-  if (end < start) return -1;
+const mostFrequent = (arr: string[]): string => {
+  const mapping: Record<string, number> = arr.reduce((acc, el) => {
+    acc[el] = acc[el] ? acc[el] + 1 : 1;
+    return acc;
+  }, {});
 
-  let mid = start + Math.floor((end - start) / 2);
+  return Object.entries(mapping).reduce(
+    (acc, el) => (el[1] > acc[1] ? el : acc),
+    [null, 0] as [string | null, number]
+  )[0] as string;
+};
 
-  if (nums[mid] === target) return mid;
-
-  if (nums[mid] > target) return binarySearch(nums, target, start, mid - 1);
-
-  return binarySearch(nums, target, mid + 1, end);
-}
-
-const nums = [2, 7, 11, 15, 34, 52, 56, 65, 74, 98];
-
-console.log(binarySearch(nums, 34, 0, nums.length - 1));
+const myStrings = ["Hello", "world", "omkar", "world", "an", "array"];
+console.log(mostFrequent(myStrings));
