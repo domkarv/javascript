1b533df522e22fe41a14aae814ddae1800104e97
Omkar Date <omkardate29@gmail.com>
2024-09-13 16:06:24 +0530

    recursive binary search

--- index.ts
+++ index.ts
@@ -1,22 +1,20 @@
-function binarySearch(nums: number[], target: number): number {
-  let start = 0;
-  let end = nums.length - 1;
+function binarySearch(
+  nums: number[],
+  target: number,
+  start: number,
+  end: number
+): number {
+  if (end < start) return -1;
 
-  while (end >= start) {
-    let mid = start + Math.floor((end - start) / 2);
+  let mid = start + Math.floor((end - start) / 2);
 
-    if (nums[mid] === target) return mid;
+  if (nums[mid] === target) return mid;
 
-    if (nums[mid] > target) {
-      end = mid - 1;
-    } else {
-      start = mid + 1;
-    }
-  }
+  if (nums[mid] > target) return binarySearch(nums, target, start, mid - 1);
 
-  return -1;
+  return binarySearch(nums, target, mid + 1, end);
 }
 
 const nums = [2, 7, 11, 15, 34, 52, 56, 65, 74, 98];
 
-console.log(binarySearch(nums, 34));
+console.log(binarySearch(nums, 34, 0, nums.length - 1));
